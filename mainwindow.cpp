68ceb3fd0a591619d966b984c272f94553741257
OverSx <32136020+OverSx@users.noreply.github.com>
2018-12-07 17:15:03 +0300

    check it!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,8 @@ MainWindow::MainWindow(QWidget *parent) :
 void MainWindow::on_btGO_clicked()
 {
         double summ = ui->lineEditSUM->text().toDouble();
+        double array[4][4] = {{1,0.013,0.012,0.015},{75.95,1, 0.88, 1.14},
+                              {84.45, 1.13, 1, 1.28},{66.88, 0.88, 0.78, 1}};
         if(summ == 0){
             ui->lbResult->setText(QString("Введите корректные значения!"));
         }
@@ -18,84 +20,8 @@ void MainWindow::on_btGO_clicked()
         int i = ui->comboBoxRUB->currentIndex();
         int j = ui->comboBoxEURO->currentIndex();
         double p, changedSumm;
-        switch(i){
-        case 0:
-            switch(j){
-            case 0:
-                p=1;
-                break;
-            case 1:
-                p = 0.013;
-                break;
-            case 2:
-                p = 0.012;
-                break;
-            case 3:
-                p = 0.015;
-                break;
-            default:
-                break;
-            }
-            changedSumm = p * summ;
-            break;
-         case 1:
-                switch(j){
-                case 0:
-                    p = 75.95;
-                    break;
-                case 1:
-                    p = 1;
-                    break;
-                case 2:
-                    p = 0.88;
-                    break;
-                case 3:
-                    p = 1.14;
-                    break;
-                default:
-                    break;
-                }
-                changedSumm = p * summ;
-            break;
-          case 2:
-                      switch(j){
-                       case 0:
-                           p = 84.45;
-                          break;
-                       case 1:
-                           p = 1.13;
-                          break;
-                       case 2:
-                           p =1;
-                           break;
-                       case 3:
-                           p = 1.28;
-                          break;
-                       default:
-                           break;
-                       }
-                      changedSumm = p * summ;
-            break;
-            case 3:
-                                  switch(j){
-                                   case 0:
-                                       p = 66.88;
-                                      break;
-                                   case 1:
-                                       p = 0.88;
-                                      break;
-                                   case 2:
-                                       p = 0.78;
-                                      break;
-                                   case 3:
-                                       p = 1;
-                                       break;
-                                   default:
-                                       break;
-                                   }
-                                  changedSumm = p * summ;
-            break;
-    }
+        p = array[i][j];
+        changedSumm = summ*p;
             ui->lbResult->setText(QString("%1").arg(changedSumm));
         }
 }
