77f5dd751894c5ab110cd844ac54bd120a37fae4
OverSx <32136020+OverSx@users.noreply.github.com>
2018-12-07 18:05:47 +0300

    check it 2.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,22 +8,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+class mistake_exception{};
+
 void MainWindow::on_btGO_clicked()
 {
-        double summ = ui->lineEditSUM->text().toDouble();
+        bool ok;
         double array[4][4] = {{1,0.013,0.012,0.015},{75.95,1, 0.88, 1.14},
                               {84.45, 1.13, 1, 1.28},{66.88, 0.88, 0.78, 1}};
-        if(summ == 0){
-            ui->lbResult->setText(QString("Введите корректные значения!"));
+        double summ = ui->lineEditSUM->text().toDouble(&ok);
+        if (ok){
+            int i = ui->comboBoxRUB->currentIndex();
+            int j = ui->comboBoxEURO->currentIndex();
+            double p, changedSumm;
+            p = array[i][j];
+            changedSumm = summ*p;
+                ui->lbResult->setText(QString("%1").arg(changedSumm));
+        } else {
+            ui->lbResult->setText("Введите корректные значения!");
         }
-        else{
-        int i = ui->comboBoxRUB->currentIndex();
-        int j = ui->comboBoxEURO->currentIndex();
-        double p, changedSumm;
-        p = array[i][j];
-        changedSumm = summ*p;
-            ui->lbResult->setText(QString("%1").arg(changedSumm));
-        }
+
 }
 
 MainWindow::~MainWindow()
